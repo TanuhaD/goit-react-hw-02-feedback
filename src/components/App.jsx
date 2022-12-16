3e138993d68bf6648c3772a6e3fe93e8954bae9d
TanuhaD <demchenkotanuha@gmail.com>
2022-12-16 11:13:40 +0200

    sdf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { Section, FeedbackOptions, Statistics, Notification } from './';
-import { Box } from './Box/Box';
 import React, { Component } from 'react';
 
 export class App extends Component {
@@ -25,34 +24,32 @@ export class App extends Component {
     return good + bad + neutral;
   };
   countPositiveFeedbackPercentage = ({ good, total }) => {
-    return ((good / total) * 100).toFixed(0);
+    return +((good / total) * 100).toFixed(0);
   };
 
   render() {
     const { good, bad, neutral, total, positivePercentage } = this.state;
     return (
       <>
-        <Box width={['100%', '400px', '700px', '1000px']}>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={['good', 'neutral', 'bad']}
-              onLeaveFeedback={this.onLeaveFeedback}
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.state.total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
             />
-          </Section>
-          <Section title="Statistics">
-            {this.state.total ? (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={total}
-                positivePercentage={positivePercentage}
-              />
-            ) : (
-              <Notification message="No feedback given" />
-            )}
-          </Section>
-        </Box>
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
       </>
     );
   }
